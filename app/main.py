9e8953128995a98ff882ef7268989fce10af2bde
Aung Ko Ko <aungkoko@ak-mb-pro.local>
2023-02-25 16:03:31 +0800

    updated API docs description

--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
 from app.src.routers.user import router as UserRouter
 
-app = FastAPI()
+
+# api descriptions
+description = """
+A Simple API to get/create/update/delete user data from a database
+"""
+app = FastAPI(title="Ryde Users", version="0.0.1", description=description)
 app.include_router(UserRouter, tags=["User"], prefix="/api/users")
 
 
